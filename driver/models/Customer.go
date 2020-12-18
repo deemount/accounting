62dd5792dffe167717ef85d1587c99500052d2bf
deemount <salvatore.gonda@web.de>
2020-12-18 21:03:31 +0100

    Added Attributes

--- driver/models/Customer.go
+++ driver/models/Customer.go
@@ -9,5 +9,6 @@ import (
 type Customer struct {
 	ID         uint           `gorm:"column:id;type:int;primaryKey;autoIncrement" json:"-"`
 	CustomerID uuid.UUID      `gorm:"column:customer_id;type:bigint" json:"customer_id"`
+	Attributes datatypes.JSON `gorm:"column:attributes" json:"attributes"`
 	Registered datatypes.Date `gorm:"column:registered;" json:"registered"`
 }
